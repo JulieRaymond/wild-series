410a6c753770f7a4e4f3ca549ca2609f43df9971
Julie <raymondjulie@proton.me>
2023-11-28 13:48:09 +0100

    quest 7 done : fixtures

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    const CATEGORIES = [
+        'Action',
+        'Comédie',
+        'Drame',
+        'Horreur',
+        'Science-fiction',
+        'Aventure',
+        'Romance',
+        'Fantaisie',
+        'Thriller',
+        'Animation'
+    ];
+
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::CATEGORIES as $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+        }
+
+        $manager->flush();
+    }
+}
